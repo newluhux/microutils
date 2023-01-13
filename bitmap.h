5f191f3dbbdcb818e50ad21e935eb33ae4793f73
LuHui <luhux76@gmail.com>
2023-01-13 20:28:21 +0800

    bitmap.h: delete unused code

--- bitmap.h
+++ bitmap.h
@@ -1,46 +1,12 @@
 #ifndef _BITMAP_H_
 #define _BITMAP_H_
 
-#include <stdio.h>
 #include <stdint.h>
-#include <errno.h>
 
 struct bitmap {
 	uint8_t *data;
 	unsigned int w;
 	unsigned int h;
 };
-
-int bitmap_check(struct bitmap *bm)
-{
-	if (bm == NULL)
-		return -EINVAL;
-	if (bm->data == NULL)
-		return -EINVAL;
-	if (bm->w == 0 || bm->h == 0)
-		return -EINVAL;
-	return 0;
-}
-
-int bitmap_view(struct bitmap *bm, char fg, char bg)
-{
-	int ret = 0;
-	ret = bitmap_check(bm);
-	if (ret < 0)
-		return ret;
-	unsigned int x, y;
-	uint8_t *bmdata = bm->data;
-	for (y = 0; y < bm->h; y++) {
-		for (x = 0; x < bm->w; x++) {
-			if (*(bmdata + y) & 1 << x) {
-				putchar(fg);
-			} else {
-				putchar(bg);
-			}
-		}
-		putchar('\n');
-	}
-	return ret;
-}
 
 #endif
