3ba7f88aab337b93d87ba9dd14a9a833d0626b00
LuHui <luhux76@gmail.com>
2023-04-04 20:52:54 +0800

    puts32.h: new puthex

--- puts32.h
+++ puts32.h
@@ -8,52 +8,30 @@
 
 #define putc putchar
 
-void putstr(char *s) {
+void putstr(char *s)
+{
 	while (*s) {
 		putc(*s);
 		s++;
 	}
 }
 
-void puthex(uint32_t n)
+void puthex(uint32_t hex)
 {
-        uint32_t val;
-        uint8_t hex[sizeof(val) * 2 + 1];
-        uint32_t i;
-
-        // split 8 bit to 4 bit, such as : 0xAF to 0x0A, 0x0F
-        for (i = 0; i < (sizeof(val) * 2); i++) {
-                val = n;
-                val >>= i * 4;
-                hex[i] = val;
-                hex[i] <<= 4; // shiftout high 4 bit
-                hex[i] >>= 4; // shiftout low  4 bit
-        }
-
-        // convert 4bit to ascii char.
-        for (i = 0; i < (sizeof(val) * 2); i++) {
-                if (hex[i] < 0xA) {
-                        hex[i] += '0';
-                } else {
-			hex[i] -= 0xA;
-                        hex[i] += 'A';
-                }
-        }
-
-	// human readable
-        uint32_t j;
-        uint8_t temp;
-        i = 0; // start of string
-        j = sizeof(val) * 2 - 1; // end of string
-        while (i < j) {
-                temp = hex[i];
-                hex[i] = hex[j];
-                hex[j] = temp;
-                i++;
-                j--;
-        }
-        hex[sizeof(val) * 2] = '\0'; // end of NUL
-        putstr((char *)hex);
+	uint32_t temp;
+	int i;
+	for (i = (sizeof(hex) * 2) - 1; i >= 0; i--) {
+		temp = hex;
+		temp &= 0xF << (i * 4);
+		temp >>= i * 4;
+		if (temp >= 0xA) {
+			temp -= 0xA;
+			temp += 'A';
+		} else {
+			temp += '0';
+		}
+		putc(temp);
+	}
 }
 
 #endif
